199a9576c8cf7195c4530d9665c956b3b0506d47
Egor Grushin <egormix1703@gmail.com>
2026-08-04 21:56:29 +0300

    26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/removeDuplicates.py
@@ -0,0 +1,9 @@
+def remove_duplicates(nums: list[int]) -> int:
+    if not nums:
+        return 0
+    slow = 0
+    for fast in range(1, len(nums)):
+        if nums[slow] != nums[fast]:
+            slow += 1
+            nums[slow] = nums[fast]
+    return slow + 1
